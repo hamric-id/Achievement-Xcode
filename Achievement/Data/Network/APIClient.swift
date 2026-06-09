629171ae1e349b53c94289f1c45478b478fccfa5
hamric-id <mhamzahr@gmail.com>
2026-06-09 22:48:52 +0700

    integrate Achivement RESTful API

---
+++ Achievement/Data/Network/APIClient.swift
@@ -0,0 +1,57 @@
+//
+//  APIClient.swift
+//  TestNBS
+//
+//  Created by Muhammad Hamzah Robbani on 09/06/26.
+//
+
+import Foundation
+
+protocol APIClientProtocol {
+    func request<T: Decodable>(_ endpoint: String, method: HTTPMethod, body: Encodable?) async throws -> T//func request<T: Decodable>(_ endpoint: String) async throws -> T
+}
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+class APIClient: APIClientProtocol {
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func request<T: Decodable>(_ endpoint: String, method: HTTPMethod = .get, body: Encodable? = nil) async throws -> T {
+        guard let url = URL(string: endpoint) else {
+            throw NetworkError.invalidURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        if let body = body {
+            request.httpBody = try JSONEncoder().encode(body)
+        }
+        
+        let (data, response) = try await session.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.invalidResponse
+        }
+        
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throw NetworkError.httpError(statusCode: httpResponse.statusCode)
+        }
+        
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            throw NetworkError.decodingError(error)
+        }
+    }
+}
---
+++ Achievement/Data/Network/NetworkError.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkError.swift
+//  TestNBS
+//
+//  Created by Muhammad Hamzah Robbani on 09/06/26.
+//
+import Foundation
+
+enum NetworkError: LocalizedError {
+    case invalidURL
+    case invalidResponse
+    case httpError(statusCode: Int)
+    case decodingError(Error)
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "Invalid URL"
+        case .invalidResponse:
+            return "Invalid response from server"
+        case .httpError(let code):
+            return "HTTP Error: \(code)"
+        case .decodingError(let error):
+            return "Decoding error: \(error.localizedDescription)"
+        }
+    }
+}
