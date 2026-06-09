05bd3bd944f5b1463fc6594dc1764dca647b19ad
hamric-id <mhamzahr@gmail.com>
2026-06-09 22:03:31 +0700

    Initial Commit

---
+++ Achievement/AchievementApp.swift
@@ -0,0 +1,17 @@
+//
+//  AchievementApp.swift
+//  Achievement
+//
+//  Created by Muhammad Hamzah Robbani on 09/06/26.
+//
+
+import SwiftUI
+
+@main
+struct AchievementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
